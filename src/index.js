078eb3b53802e0910dfad522e84e7e1cc81a5e08
Dratatin <matchirouze@orange.fr>
2022-07-29 16:35:27 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style/main.css';
+import Home from './page/Home';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+);
+
---
+++ src/page/Home.js
@@ -0,0 +1,9 @@
+import logo from '../assets/logo.svg';
+
+function Home() {
+  return (
+    <img src={logo} className="App-logo" alt="logo" />
+  )
+}
+
+export default Home
